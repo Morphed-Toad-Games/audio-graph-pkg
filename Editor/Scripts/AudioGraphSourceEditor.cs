7e4e8f6b9b7fbb09f1ac8b9981328635869597b5
Joe Constable <joeconstable97@gmail.com>
2024-07-16 23:14:23 +0100

    Add editor folder

---
+++ Editor/Scripts/AudioGraphSourceEditor.cs
@@ -0,0 +1,65 @@
+using Josephus.AudioGraph;
+using UnityEditor;
+
+[CustomEditor(typeof(AudioGraphSource))]
+public class AudioGraphSourceEditor : Editor
+{
+    AudioGraphSource audioGraphSource;
+
+    private void Awake()
+    {
+        audioGraphSource = (AudioGraphSource)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        var selectedGraph = (AudioGraph)EditorGUILayout.ObjectField("AudioGraph Blueprint", audioGraphSource.AudioGraphBlueprint, typeof(AudioGraph), false);
+        if (selectedGraph != audioGraphSource.AudioGraphBlueprint)
+        {
+            audioGraphSource.AudioGraphBlueprint = selectedGraph;
+
+            if (selectedGraph == null)
+                OnGraphCleared();
+            else
+            {
+                OnGraphChanged();
+            }
+        }
+
+        if (selectedGraph != null)
+            DrawGraph(selectedGraph);
+    }
+
+    void DrawGraph(AudioGraph graph)
+    {
+        EditorGUILayout.Separator();
+
+        EditorGUI.BeginChangeCheck();
+
+        foreach (var parameter in audioGraphSource.GraphInstanceParameters)
+        {
+            parameter.Value = EditorGUILayout.FloatField(parameter.Name, parameter.Value);
+        }
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            EditorUtility.SetDirty(audioGraphSource);
+        }
+
+        EditorGUILayout.Separator();
+
+        EditorGUILayout.HelpBox($"Pooled audio sources: {audioGraphSource.GetAudioSourcePoolCount()}", MessageType.Info);
+    }
+
+    void OnGraphChanged()
+    {
+        audioGraphSource.Apply();
+        EditorUtility.SetDirty(audioGraphSource);
+    }
+
+    void OnGraphCleared()
+    {
+        audioGraphSource.Apply();
+        EditorUtility.SetDirty(audioGraphSource);
+    }
+}
